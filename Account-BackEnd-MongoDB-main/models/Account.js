6735570de3004ff763a35cabad7fc31730dafd8c
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-10-09 10:19:44 +0200

    sidenotes

--- Account-BackEnd-MongoDB-main/models/Account.js
+++ Account-BackEnd-MongoDB-main/models/Account.js
@@ -1,27 +1,44 @@
+// Bibliothek namens "mongoose" für Datenbank-Dinge.
 import mongoose from "mongoose";
 
+// Wir erstellen ein Schema für einen "Account".
 const accountSchema = new mongoose.Schema({
+  // Der Name .
   name: {
     type: String,
-    required: [true, "name is required"],
-    unique: true, //  Das Feld erfordert eindeutige Werte, keine Duplikate sind erlaubt.
-    trim: true, // Entfernt führende und nachfolgende Leerzeichen aus dem Wert, um Datenkonsistenz sicherzustellen.
+    required: [true, "Name is required"],
+    unique: true, // Jeder Name sollte einmalig sein.
+    trim: true, // Wir entfernen Leerzeichen am Anfang und Ende des Namens.
   },
+  // Der Vorname 
   first_name: {
     type: String,
-    required: [true, "First-Name is required"],
-    unique: true,
-    trim: true,
+    required: [true, "First Name is required"],
+    unique: true, // Jeder Vorname sollte einmalig sein.
+    trim: true, // Wir entfernen Leerzeichen am Anfang und Ende des Vornamens.
   },
+  // Die E-Mail 
   email: {
     type: String,
-    required: [true, "E-Mail is required"],
-    unique: true,
-    trim: true,
+    required: [true, "Email is required"],
+    unique: true, // Jede E-Mail sollte einmalig sein.
+    trim: true, // Wir entfernen Leerzeichen am Anfang und Ende der E-Mail.
+
+    // Hier um sicherzustellen, dass die E-Mail wirklich ist.
+    validate: { //validate-Option in der Mongoose-Bibliothek 
+      validator: function (value) {
+        // Wir benutzen einen besonderen Code (Regulärer Ausdruck), um die E-Mail zu überprüfen.
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailRegex.test(value); // Wir überprüfen, ob die E-Mail passt.
+      },
+      message: "Invalid email address", // Wenn die E-Mail nicht passt, sagen wir "Ungültige E-Mail-Adresse".
+    },
   },
+  // Ein Bild kann gespeichert werden, aber es ist nicht zwingend erforderlich.
   image: {
-    type: String, // Speichern Sie den Dateipfad zum hochgeladenen Bild.
+    type: String, // Hier können wir den Dateipfad zu einem Bild speichern, wenn wir möchten.
   },
 });
 
+// Wir exportieren dieses Schema, damit wir es später verwenden können.
 export default mongoose.model("Account", accountSchema);
--- Account-BackEnd-MongoDB-main/routes/accountsRouter.js
+++ Account-BackEnd-MongoDB-main/routes/accountsRouter.js
@@ -1,19 +1,50 @@
+// Importiere das Express-Framework
 import express from "express";
+
+// Importiere den accountController-Datei als Modul
 import * as accountController from "../controllers/accountController.js";
 
+// Erstelle einen Router für die Accounts-Routen
 const accountsRouter = express.Router();
 
+// Definiere die Routen und ihre entsprechenden Controller-Methoden
+
+// Route: /accounts
 accountsRouter
-.route("/")
-.get(accountController.getAllAccounts)
-.post(accountController.addNewAccount);
+  .route("/")
+  .get(accountController.getAllAccounts) // GET-Anfrage ruft die Methode `getAllAccounts` im `accountController` auf
+  .post(accountController.addNewAccount); // POST-Anfrage ruft die Methode `addNewAccount` im `accountController` auf
 
+// Route: /accounts/:id
 accountsRouter
-.route("/:id")
-.get(accountController.getAccountById)
-.put(accountController.updateAccount)
-.delete(accountController.deleteAccount);
+  .route("/:id")
+  .get(accountController.getAccountById) // GET-Anfrage ruft die Methode `getAccountById` im `accountController` auf
+  .put(accountController.updateAccount) // PUT-Anfrage ruft die Methode `updateAccount` im `accountController` auf
+  .delete(accountController.deleteAccount); // DELETE-Anfrage ruft die Methode `deleteAccount` im `accountController` auf
 
-accountsRouter.patch("/id/addTag", accountController.addNewAccount);
+// Route: /accounts/id/addTag
+accountsRouter.patch("/id/addTag", accountController.addNewAccount); // PATCH-Anfrage ruft die Methode `addNewAccount` im `accountController` auf
 
+// Exportiere den erstellten Router, um ihn in anderen Teilen der Anwendung zu verwenden
 export default accountsRouter;
+
+{
+  /* 
+ Definition der Routen und ihrer Controller-Methoden:
+ 
+    Es werden zwei Haupt-Routen definiert: "/accounts" und "/accounts/:id".
+    Jede dieser Routen hat spezifische HTTP-Verben und Controller-Methoden:
+    GET: Ruft Daten ab.
+    POST: Erstellt neue Daten.
+    PUT: Aktualisiert vorhandene Daten.
+    DELETE: Löscht Daten.
+Die zugehörigen Controller-Methoden werden aus dem `accountController`-Modul aufgerufen, um die entsprechenden Aufgaben auszuführen.
+
+    Import des accountController-Moduls:
+
+    import * as accountController from "../controllers/accountController.js";
+
+Hier wird das Modul accountController importiert, das verschiedene Controller-Methoden enthält, um Anfragen für die "Accounts" zu verarbeiten.
+
+*/
+}
