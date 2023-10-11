482ea63df12807007004eca5f33382a3aa77b3f6
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-10-11 23:43:36 +0200

    debugging and design css

--- Client/src/components/Chat/ChatWindow.jsx
+++ Client/src/components/Chat/ChatWindow.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./ChatWindow.css";
 
 function ChatWindow() {
@@ -12,8 +12,11 @@ function ChatWindow() {
       user: "Anderer Benutzer",
       message: "Hi, wie geht's?",
     },
-    // Weitere Nachrichten hier
   ]);
+
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chatWindowRef = useRef(null);
 
   const handleSendMessage = () => {
     if (newMessage.trim() === "") return;
@@ -24,21 +27,48 @@ function ChatWindow() {
     ];
     setChatMessages(updatedMessages);
     setNewMessage("");
+
+    setIsTyping(true);
+
+    setTimeout(() => {
+      setIsTyping(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
+  }, [chatMessages]);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
+    }
   };
 
   return (
     <>
-      <div className="chat">
+      <div className="chat" ref={chatWindowRef}>
         {chatMessages.map((message, index) => (
           <div
             key={index}
             className={`user-info ${message.user === "Du" ? "mine" : "yours"}`}>
-            <img src="URL_DES_AVATARS" alt="Avatar" />
+            <img
+              src="https://a.storyblok.com/f/178900/2880x1620/1ac38ba19d/8aa767c3aa80a9185f7614adbba3982d1625731728_main.jpg/m/filters:quality(95)format(webp)"
+              alt="Avatar"
+            />
+            <div className="user-name">{message.user}</div>
             <div className="user-messages">
               <div className="message">{message.message}</div>
             </div>
           </div>
         ))}
+        {isTyping && (
+          <div className="typing-indicator-container">
+            <div className="typing-indicator" />
+            <div className="typing-indicator" />
+            <div className="typing-indicator" />
+          </div>
+        )}
       </div>{" "}
       <div className="new-message">
         <input
@@ -46,9 +76,10 @@ function ChatWindow() {
           placeholder="Neue Nachricht"
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
-        />
-      </div>{" "}
-      <button onClick={handleSendMessage}>Senden</button>
+          onKeyPress={handleKeyPress}
+        />{" "}
+        <button onClick={handleSendMessage}>Senden</button>{" "}
+      </div>
     </>
   );
 }
